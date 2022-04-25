389b6fef75fa3000234dab057225e169814960f1
PeryLoth <www.luisplata@gmail.com>
2022-04-25 12:54:12 -0500

    port 3000

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ app.post('/interactions', async function (req, res) {
   }
 });
 
-app.listen(80, () => {
+app.listen(3000, () => {
   console.log('Listening on port 3000');
   // Check if guild commands from commands.json are installed (if not, install them)
   HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [
